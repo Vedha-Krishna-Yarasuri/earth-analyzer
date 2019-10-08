c26ef7323e27abd3c8393250c078934c28764e7d
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-10-08 22:28:12 +0530

    Update transmitter.ino

--- transmitter.ino
+++ transmitter.ino
@@ -1,11 +1,13 @@
-#include <SPI.h>
+ #include <SPI.h>
 #include <nRF24L01.h>
 #include <printf.h>
 #include <RF24.h>
 #include <RF24_config.h>
 
+void recieve_values();
+
 RF24 radio(7, 8); // CSN, CE
-const byte address[6] = "00001";
+const byte addresses[][6] = {"00001", "00002"};
 
 int x_key = A1;                                              
 int y_key = A0;                                              
@@ -18,12 +20,13 @@ int q_pos;
 
 void setup() {
   radio.begin();
-  radio.openWritingPipe(address);
+  radio.openWritingPipe(addresses[1]); // 00002
+  radio.openReadingPipe(1, addresses[0]); // 00001
   radio.setPALevel(RF24_PA_MIN);
   radio.setChannel(87);
   radio.setDataRate(RF24_250KBPS);
+  recieve_values();
   radio.stopListening();
- 
   Serial.begin(9600);
   pinMode (x_key, INPUT) ;                    
   pinMode (y_key, INPUT) ;
@@ -84,4 +87,19 @@ else if(p_pos<400 || p_pos>600)
   }
   delay(100);
 }
+}
+
+void recieve_values()
+{
+      radio.startListening();
+      while(radio.available())
+      {
+        // code to read the temperature values sent by the earth analyzer
+        // radio.read(&temperature, sizeof(temperature));
+        //  Serial.println(temperature);
+        
+        // logic to be named for writing the class names
+        //Serial.print();
+      }
+      delay(10);
 }
